ab8b0c52b5c30a11e81a180951e06de5d7962d35
Gina Dimino <gdimino@google.com>
2015-03-19 13:29:36 -0700

    docs: Added staging script and updated app.yaml for Python 2.7.
    bug: 18631527
    Change-Id: I8fc0af684184949e1d4022dd74c17821c9ab400c

---
+++ src/stageme.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+#
+# How to use:
+# From directory {root}/out/target/common/docs/online-sac run:
+#    $ ./stageme.sh <server number>
+#
+# For example, to stage on
+# https://sourceandroid-staging13.googleplex.com/
+# run
+#    $ ./stageme.sh 13
+#
+
+echo  'Please run this script from the output directory where it lives: out/target/common/docs/online-sac'
+echo ' '
+
+# Go up a directory to stage content
+cd ..
+
+# Edit the app.yaml file to upload to the specified server.
+sed 's/staging[0-9]*$/staging'$1'/' online-sac/app.yaml >  .temp
+
+# Confirm app.yaml content
+echo 'Here is the revised app.yaml for your staging server:'
+cat .temp
+
+while true; do
+read -p "Is this app.yaml OK to stage? " yn
+    case $yn in
+        [Yy]* ) break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+# Copy in new app.yaml content
+cp .temp online-sac/app.yaml
+rm .temp
+
+# Stage the data on the server.
+/google/data/ro/projects/apphosting/tools/appcfg_over_stubby.par update online-sac
+
+# Returning to original directory
+cd online-sac
+
+
+echo 'Your staged content is available at https://sourceandroid-staging'$1'.googleplex.com/'
+
+
+
