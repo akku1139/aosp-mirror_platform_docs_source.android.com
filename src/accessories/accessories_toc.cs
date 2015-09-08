0812316d26591188c3534ab35c7c9d24ca32c935
Heidi von Markham <hvm@google.com>
2015-09-08 16:12:21 -0700

    Docs: Edits to accessory pages, 80 char limit, standardized AOAP refs
    Bug: 23624707
    Change-Id: I437c81961729cb1972215765cb45372002ef96df

--- src/accessories/accessories_toc.cs
+++ src/accessories/accessories_toc.cs
@@ -1,5 +1,5 @@
 <!--
-    Copyright 2014 The Android Open Source Project
+    Copyright 2015 The Android Open Source Project
 
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
@@ -43,7 +43,7 @@
         </ul>
       </li>
         </ul>
-      </li>    
+      </li>
   <li class="nav-section">
 <div class="nav-section-header">
       <a href="<?cs var:toroot ?>accessories/custom.html">
@@ -52,12 +52,12 @@
     </div>
     <ul>
       <li class="nav-section">
-        <div class="nav-section-header"><a href="<?cs var:toroot ?>accessories/protocol.html"><span class="en">Open Accessory Protocol</span>
+        <div class="nav-section-header"><a href="<?cs var:toroot ?>accessories/protocol.html"><span class="en">AOAP</span>
         </a>
         </div>
         <ul>
-          <li><a href="<?cs var:toroot ?>accessories/aoa2.html">Version 2.0</a></li>
-          <li><a href="<?cs var:toroot ?>accessories/aoa.html">Version 1.0</a></li>
+          <li><a href="<?cs var:toroot ?>accessories/aoa2.html">AOAP 2.0</a></li>
+          <li><a href="<?cs var:toroot ?>accessories/aoa.html">AOAP 1.0</a></li>
         </ul>
         </li>
        </ul>
