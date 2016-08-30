9d89ec5992c92baca0be7ba3b5d19cf13a4060e0
Clay Murphy <claym@google.com>
2016-08-29 17:16:06 -0700

    Docs: Splitting Source tab contents into subdirectories
    Bug: 27461598
    Change-Id: If57710015e6725c0b5878f520a45bb4a58808da0

--- src/source/source_toc.cs
+++ src/source/source_toc.cs
@@ -21,70 +21,63 @@
       </a>
     </div>
     <ul>
-      <li><a href="<?cs var:toroot ?>source/code-lines.html">Codelines, Branches, and Releases</a></li>
-      <li><a href="<?cs var:toroot ?>source/build-numbers.html">Codenames, Tags, and Build Numbers</a></li>
-      <li><a href="<?cs var:toroot ?>source/roles.html">Project Roles</a></li>
-      <li><a href="<?cs var:toroot ?>source/brands.html">Brand Guidelines</a></li>
-      <li><a href="<?cs var:toroot ?>source/licenses.html">Licenses</a></li>
-      <li><a href="<?cs var:toroot ?>source/faqs.html">FAQ</a></li>
+      <li><a href="<?cs var:toroot ?>source/overview/code-lines.html">Codelines, Branches, and Releases</a></li>
+      <li><a href="<?cs var:toroot ?>source/overview/build-numbers.html">Codenames, Tags, and Build Numbers</a></li>
+      <li><a href="<?cs var:toroot ?>source/overview/roles.html">Project Roles</a></li>
+      <li><a href="<?cs var:toroot ?>source/overview/brands.html">Brand Guidelines</a></li>
+      <li><a href="<?cs var:toroot ?>source/overview/licenses.html">Licenses</a></li>
+      <li><a href="<?cs var:toroot ?>source/overview/faqs.html">FAQ</a></li>
     </ul>
   </li>
 
 
   <li class="nav-section">
       <div class="nav-section-header">
-        <a href="<?cs var:toroot ?>source/requirements.html">
+        <a href="<?cs var:toroot ?>source/build/index.html">
           <span class="en">Downloading and Building</span>
         </a>
       </div>
        <ul>
-        <li><a href="<?cs var:toroot ?>source/initializing.html">Establishing a Build Environment</a></li>
-        <li><a href="<?cs var:toroot ?>source/downloading.html">Downloading the Source</a></li>
-        <li><a href="<?cs var:toroot ?>source/building.html">Preparing to Build</a></li>
-        <li><a href="<?cs var:toroot ?>source/jack.html">Compiling with Jack</a></li>
-        <li><a href="<?cs var:toroot ?>source/devices.html">Selecting Devices</a></li>
-        <li><a href="<?cs var:toroot ?>source/running.html">Running Builds</a></li>
-        <li><a href="<?cs var:toroot ?>source/building-kernels.html">Building Kernels</a></li>
-        <li><a href="<?cs var:toroot ?>source/known-issues.html">Known Issues</a></li>
+        <li><a href="<?cs var:toroot ?>source/build/initializing.html">Establishing a Build Environment</a></li>
+        <li><a href="<?cs var:toroot ?>source/build/downloading.html">Downloading the Source</a></li>
+        <li><a href="<?cs var:toroot ?>source/build/building.html">Preparing to Build</a></li>
+        <li><a href="<?cs var:toroot ?>source/build/jack.html">Compiling with Jack</a></li>
+        <li><a href="<?cs var:toroot ?>source/build/devices.html">Selecting Devices</a></li>
+        <li><a href="<?cs var:toroot ?>source/build/running.html">Running Builds</a></li>
+        <li><a href="<?cs var:toroot ?>source/build/building-kernels.html">Building Kernels</a></li>
+        <li><a href="<?cs var:toroot ?>source/build/known-issues.html">Known Issues</a></li>
       </ul>
   </li>
   <li class="nav-section">
           <div class="nav-section-header">
-            <a href="<?cs var:toroot ?>source/developing.html">
+            <a href="<?cs var:toroot ?>source/develop/index.html">
               <span class="en">Developing</span>
             </a>
           </div>
           <ul>
-            <li><a href="<?cs var:toroot ?>source/using-repo.html">Using Repo</a></li>
-            <li><a href="<?cs var:toroot ?>source/git-resources.html">Learning Git</a></li>
-            <li><a href="<?cs var:toroot ?>source/add-device.html">Adding a New Device</a></li>
-            <li><a href="<?cs var:toroot ?>source/64-bit-builds.html">Understanding 64-bit Builds</a></li>
+            <li><a href="<?cs var:toroot ?>source/develop/using-repo.html">Using Repo</a></li>
+            <li><a href="<?cs var:toroot ?>source/develop/git-resources.html">Learning Git</a></li>
+            <li><a href="<?cs var:toroot ?>source/develop/add-device.html">Adding a New Device</a></li>
+            <li><a href="<?cs var:toroot ?>source/develop/64-bit-builds.html">Understanding 64-bit Builds</a></li>
           </ul>
   </li>
 
   <li class="nav-section">
     <div class="nav-section-header">
-      <a href="<?cs var:toroot ?>source/contributing.html">
+      <a href="<?cs var:toroot ?>source/contribute/index.html">
         <span class="en">Contributing</span>
       </a>
     </div>
        <ul>
-        <li><a href="<?cs var:toroot ?>source/life-of-a-patch.html">Life of a Patch</a>
-        <li><a href="<?cs var:toroot ?>source/submit-patches.html">Submitting Patches</a></li>
+        <li><a href="<?cs var:toroot ?>source/contribute/life-of-a-patch.html">Life of a Patch</a>
+        <li><a href="<?cs var:toroot ?>source/contribute/submit-patches.html">Submitting Patches</a></li>
         <li><a href="http://android-review.googlesource.com">View Patches</a></li>
-        <li><a href="<?cs var:toroot ?>source/life-of-a-bug.html">Life of a Bug</a></li>
-        <li><a href="<?cs var:toroot ?>source/report-bugs.html">Reporting Bugs</a></li>
-        <li><a href="<?cs var:toroot ?>source/read-bug-reports.html">Reading Bug Reports</a></li>
-        <li><a href="<?cs var:toroot ?>source/code-style.html">Code Style Rules</a></li>
+        <li><a href="<?cs var:toroot ?>source/contribute/life-of-a-bug.html">Life of a Bug</a></li>
+        <li><a href="<?cs var:toroot ?>source/contribute/report-bugs.html">Reporting Bugs</a></li>
+        <li><a href="<?cs var:toroot ?>source/contribute/read-bug-reports.html">Reading Bug Reports</a></li>
+        <li><a href="<?cs var:toroot ?>source/contribute/code-style.html">Code Style Rules</a></li>
+        <li><a href="<?cs var:toroot ?>source/contribute/community.html">Community</a></li>
       </ul>
-  </li>
-
-  <li class="nav-section">
-    <div class="nav-section-header empty">
-      <a href="<?cs var:toroot ?>source/community.html">
-        <span class="en">Community</span>
-      </a>
-    </div>
   </li>
 
 </ul>
