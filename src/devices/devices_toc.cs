0eecd8a01fda0b6a358a61de378203264c98c3fe
Clay Murphy <claym@google.com>
2016-11-28 11:49:29 -0800

    Docs: Add Retail Demo Mode to Display Settings
    Test: make online-sac-docs and staged on instance #3
    
    Bug: 31591584
    Change-Id: Ia8e7190c7ee29740172d6e3ce4db754deba5022c

--- src/devices/devices_toc.cs
+++ src/devices/devices_toc.cs
@@ -403,6 +403,7 @@
           <li><a href="<?cs var:toroot ?>devices/tech/display/hdr.html">HDR Video</a></li>
           <li><a href="<?cs var:toroot ?>devices/tech/display/launcher-shortcuts.html">Launcher Shortcuts</a></li>
           <li><a href="<?cs var:toroot ?>devices/tech/display/night-light.html">Night Light</a></li>
+          <li><a href="<?cs var:toroot ?>devices/tech/display/retail-mode.html">Retail Demo Mode</a></li>
         </ul>
       </li>
 
